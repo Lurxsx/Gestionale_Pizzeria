41fc5d0139d15c1600e4a9edc100d87d18282f05
Lorenzo20063 <lorenzo.varric@gmail.com>
2024-01-31 15:26:03 +0100

    correzzione funzionalità di aggiunta ordine

--- src/main/java/net/piramide/gestionale_pizzeria/TakeOrderController.java
+++ src/main/java/net/piramide/gestionale_pizzeria/TakeOrderController.java
@@ -144,7 +144,7 @@ public class TakeOrderController {
     }
 
     public void onConfirmButtonClick(ActionEvent event) throws IOException {
-        if(txtCity.getText() != null && txtIndirizzo.getText() != null && txtNom.getText() != null && txtTel.getText() != null /*&& listaPizze.size() > 1*/) {
+        if(!txtCity.getText().equals("") && !txtIndirizzo.getText().equals("") && !txtNom.getText().equals("") && !txtTel.getText().equals("")) {
             Ordine Ordine = new Ordine(listaPizze, txtNom.getText(),txtIndirizzo.getText(), txtNom.getText(), 1);
             Sistema.make_Order(Ordine);
             System.out.println("Il sistema ha ora " + Sistema.getCountOrdini() + " ordini");
